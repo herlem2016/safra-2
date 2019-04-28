a889106de921922fa44acde23dde5f68b803c76c
Emmanuel <herlem.kx@gmail.com>
2019-04-28 13:32:56 -0500

    fff

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function MostrarOpcionesHabilitadas(evitarToggle) {
         }
 
 function ContinuarPagando() {
-    window.cordova.InAppBrowser.open(url + 'logic/controlador.aspx?op=PresentarPagador', "_blank");
+    window.location.href = url + 'logic/controlador.aspx?op=PresentarPagador';
 }
 
         function PintarItem(catalogo, clave, xmlDoc0){
