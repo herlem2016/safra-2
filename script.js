e43335623d0e4851c03d5deab6298b6835434b4f
Emmanuel <herlem.kx@gmail.com>
2019-11-25 11:10:04 -0600

    ok

--- script.js
+++ script.js
@@ -221,7 +221,7 @@ function RegistrarVariables(datos, xmlDoc) {
     window.localStorage.setItem("fracc_", GetValor(xmlDoc, "fraccionamiento"));
     window.localStorage.setItem("email_", datos[0].value);
     window.localStorage.setItem("contrasena_", datos[1].value);
-    window.localStorage.setItem("domicilio", GetValor(xmlDoc, "domicilio").split(','));
+    window.localStorage.setItem("domicilios", GetValor(xmlDoc, "domicilios"));
     document.getElementById("nombre-usuario").innerHTML = GetValor(xmlDoc, "nombre");
     document.getElementById("u-fraccionamiento").innerHTML = GetValor(xmlDoc, "s_nfracc");
     document.getElementById("u-domicilio").innerHTML = GetValor(xmlDoc, "s_domicilio");
@@ -292,8 +292,9 @@ function RegistrarNotificaciones() {
                 UnSuscribir();
             });
             FCMPlugin.subscribeToTopic('FRA_1_' + window.localStorage.getItem("codigoActivacion"));
-            for(var k=0;k<window.localStorage.getItem("domicilio").length;k++){
-				FCMPlugin.subscribeToTopic('FRA_1_' + window.localStorage.getItem("codigoActivacion") + "-dom_" + window.localStorage.getItem("domicilio")[k]);
+            var domicilios = window.localStorage.getItem("domicilios").split(",");
+            for(var k=0;k<domicilios.length;k++){
+				FCMPlugin.subscribeToTopic('FRA_1_' + window.localStorage.getItem("codigoActivacion") + "-dom_" + domicilios[k]);
 			}
             FCMPlugin.onNotification(function (data) {
                 document.getElementById("notifi-audio").play();
